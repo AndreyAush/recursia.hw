898688146e9efb68fe78bc9899a42735a3d04d4d
Andrey <bakllorik90mail.ru>
2024-02-23 23:17:42 +0800

    task1, hw7

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void ShowNumbers (int start, int end)
+{
+ // Базовый случай
+ if (start == end)
+ {
+ Console.Write(start);
+ return;
+ }
+ // Рекурсивный случай
+ Console.Write(start + " ");
+ShowNumbers(start + 1, end);
+}
+Console.Write("Введите число M: ");
+ int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ ShowNumbers(M, N); // start = M, end = N
