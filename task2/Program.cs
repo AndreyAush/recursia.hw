dc997f5860710a05c5545b54f6a73848959fe0f1
Andrey <bakllorik90mail.ru>
2024-02-24 13:25:31 +0800

    task2 hw7

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int ack(int m, int n) 
+{ 
+    if (m == 0)
+    { 
+        return n + 1; 
+    } 
+    else if((n == 0))
+    { 
+        return ack(m - 1, 1); 
+    } 
+    else return ack(m - 1, ack(m, n - 1));
+} 
+  
+Console.Write("Введите число m: ");
+ int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ack(m, n)); 
