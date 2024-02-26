e33614966c86dada67d341edbb1af66ed2d0a051
Andrey <bakllorik90mail.ru>
2024-02-26 14:51:55 +0800

    add task 3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void PrintReserveArray (int[] array, int j)
+{
+    if (j == 0)
+    {
+        Console.Write(array[j]);
+       return;
+    }
+    //рекурсивный случай
+    Console.Write(array[j] + " ");
+    PrintReserveArray(array, j - 1);
+}
+int[] array = { 1, 2, 3, 4 };
+int j = array.Length - 1; 
+PrintReserveArray(array, j);
